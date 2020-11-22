96bfa0abaeac65cc7e3980822c4fe4a47f82cce0
Francisndungu2 <francianmbugua@gmail.com>
2020-11-22 19:37:33 +0300

    add canonical

--- src/org/arpit/CaesarCipher.java
+++ src/org/arpit/CaesarCipher.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class CaesarCipher{
 
     public static void main(String[]args){
-
-
+        Scanner sc=new Scanner(System.in);
+        int shift,i,n;
+        String str;
     }
 }
