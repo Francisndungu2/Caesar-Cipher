1977ba80d57ef98bb4013313309d6128e98a2d87
Francisndungu2 <francianmbugua@gmail.com>
2020-11-22 21:05:25 +0300

    add decription

--- src/org/arpit/CaesarCipher.java
+++ src/org/arpit/CaesarCipher.java
@@ -1,9 +1,10 @@
 package org.arpit;
-
 import java.util.Scanner;
-public class CaesarCipher{
 
-    public static void main(String[]args){
+public class CaesarCipher
+{
+    public static void main(String[] args)
+    {
         Scanner sc=new Scanner(System.in);
         int shift,i,n;
         String str;
@@ -17,6 +18,7 @@ public class CaesarCipher{
         char ch3,ch4;
         System.out.println("Enter the value by which each letter of the string is to be shifted");
         shift=sc.nextInt();
+
         System.out.println();
         System.out.println("Encrypted text is");
         for(i=0;i<n;i++)
@@ -26,7 +28,6 @@ public class CaesarCipher{
                 str1.append(ch3);
             }
             else if(ch1[i]==' ')
-
             {
                 str1.append(ch1[i]);
             }
@@ -35,12 +36,29 @@ public class CaesarCipher{
 
         System.out.println();
         System.out.println("Decrypted text is");
+
         char[] ch2 = str1.toString().toCharArray();
         for(i=0;i<str1.length();i++)
         {
+            if(Character.isLetter(ch2[i]))
+            {
+                if(((int)ch2[i]-shift)<97)
+                {
+                    ch4=(char)(((int)ch2[i]-shift-97+26)%26+97);
 
-    }
+                }
+                else
+                {
+                    ch4=(char)(((int)ch2[i]-shift-97)%26+97);
+                }
+                str2.append(ch4);
             }
 
+            else if(ch2[i]==' ')
+            {
+                str2.append(ch2[i]);
+            }
+        }
+        System.out.println(str2);
     }
 }
